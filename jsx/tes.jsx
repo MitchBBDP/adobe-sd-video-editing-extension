56ccf824b11682c0ac79bdcc9936c30e7389838a
Mitchell Panahon <mitchell_panahon@yahoo.com>
2023-11-30 01:53:29 +0400

    Refactor for Import Files to Prproj

--- jsx/tes.jsx
+++ jsx/tes.jsx
@@ -1,15 +1,16 @@
 ﻿//@include "utils.jsx"
-// createNewProject("alba re qwe", true);
+createNewProject("1qwe qwe mia", "25 November 2023", true);
 
 function createNewProject(nameOfTandem, currentDate, hasBoard) {
-    utils.activeProjectInitialize();
+    var ap = new ActiveProject();
+    var srcFolder = ap.getSrcFolder();
 
-    var fileName = utils.fileNameChecker();
-    var fileNameLength = fileName.split(/\s+/).length;
-    var parentFolderPath = projPath.substring(0, projPath.lastIndexOf("\\") + 1);
-    var parentFolderObj = new Folder(parentFolderPath);
+    //Set Properties for Default Untitled PrProj
+    ap.InitializeCurrentProject();
 
     //Check if you are in the default untitled prproj
+    var fileName = ap.getFileName();
+    var fileNameLength = fileName.split(/\s+/).length;
     if (fileName.toLowerCase() != 'untitled' || fileNameLength > 1) {
         var userConfirmed = confirm("Do you want to proceed?", true, "Default Untitled Project Not Selected");
 
@@ -19,53 +20,67 @@ function createNewProject(nameOfTandem, currentDate, hasBoard) {
     }
 
     //Tandem folder creation and error handler
-    var isTandemFolderCreated = createTandemFolder(nameOfTandem, parentFolderObj);
-    if (isTandemFolderCreated == "error") {
+    var tandemFolder = ap.createNewFolder(nameOfTandem);
+    if (!tandemFolder) {
         return;
-    } else {
-        var tandemFolderObj = isTandemFolderCreated[0];
-        var videosFolderObj = isTandemFolderCreated[1];
-        var tandemProjectFilePath = tandemFolderObj.fsName + "/" + nameOfTandem;
     }
 
     //File import and error handler
-    var isFilesImported = importTandem(tandemProjectFilePath, videosFolderObj, hasBoard);
-    if (isFilesImported === "Invalid Selection") {
-        videosFolderObj.remove();
-        tandemFolderObj.remove();
+    var tandemProjectFilePath = tandemFolder.fsName + "/" + nameOfTandem;
+    var binFolder = "Videos";
+    var fileSelected = ap.fileSelector();
+
+    //Check if file selection is valid
+    if (!fileSelected || !ap.selectionValidator(binFolder, fileSelected.length, hasBoard)) {
+        tandemFolder.remove();
         return;
     }
 
-    //Import FHD Stock Files to Project
-    var fhdFolder = srcFolder + "/" + "fhd";
-    var fhdFolderObj = new Folder(fhdFolder);
-    utils.importStock(fhdFolderObj, templateBin);
+    //Create New Tandem Premiere Pro Project File (Prproj)
+    app.newProject(tandemProjectFilePath);
+
+    //Set Properties for Current Tandem PrProj
+    ap.InitializeCurrentProject();
+
+    ap.projRoot.createBin("Template");
+    ap.projRoot.createBin("Videos");
+    ap.projRoot.createBin("Sub Clips");
+    ap.fhdBinInitialize();
+
+    //Copy Files in a new folder inside the tandem folder
+    var videosFolder = ap.copyFilesToSubFolder(fileSelected, binFolder);
+
+    //Import Copied Files to PrProj
+    ap.importToProject(videosFolder.getFiles(), ap.vidBin);
+
+    //Import FHD Stock Files to PrProj
+    var stockFolder = new Folder(srcFolder + "/fhd");
+    ap.importToProject(stockFolder.getFiles(), ap.templateBin);
 
     //Initialize variables for stock template items
-    utils.fhdTemplateToVarInitialize();
+    ap.fhdTemplateToVarInitialize();
 
     //Initialize variables for tandem videos
-    utils.fhdVidsToVarInitialize(hasBoard);
+    ap.fhdVidsToVarInitialize(hasBoard);
 
     //Rename project files
     renameProjectFiles(hasBoard);
 
     //Create subclips to remove audio
-    fhdIntroMask = utils.convertToSubclip(fhdIntroMask);
-    walkToPlane = utils.convertToSubclip(walkToPlane);
-    freefall = utils.convertToSubclip(freefall);
-    if (hasLanding) {
-        landing = utils.convertToSubclip(landing);
+    ap.fhdIntroMask = ap.convertToSubclip(ap.fhdIntroMask);
+    ap.walkToPlane = ap.convertToSubclip(ap.walkToPlane);
+    ap.freefall = ap.convertToSubclip(ap.freefall);
+    if (ap.hasLanding) {
+        ap.landing = ap.convertToSubclip(ap.landing);
     }
 
     //Create New Sequence for FHD
-    var seqFolder = srcFolder + "/" + "sequence";
-    var fhdSqPresetPath = seqFolder + "/" + "FullHD.sqpreset";
+    var fhdSqPresetPath = srcFolder + "/" + "sequence" + "/" + "FullHD.sqpreset";
     var fhdSqPreset = new File(fhdSqPresetPath);
     createFhdSeq(nameOfTandem, fhdSqPreset);
 
     //Initialize Tracks
-    utils.activeSeqAndTracksInitialize();
+    ap.activeSeqAndTracksInitialize();
 
     //Insert clips to FHD sequence
     insertFhdClips();
@@ -76,7 +91,7 @@ function createNewProject(nameOfTandem, currentDate, hasBoard) {
     //Lower Thirds
     var lowerThirdsMogrtPath = mogrtFolder + "/" + "Lower Thirds.mogrt";
     var lowerThirdsMogrtFile = new File(lowerThirdsMogrtPath);
-    var lowerThirdsStartTime = preInterviewVClip[0].start.seconds + 2;
+    var lowerThirdsStartTime = ap.preInterviewVClip[0].start.seconds + 2;
     var lowerThirdsClip = insertMogrts(lowerThirdsMogrtFile, lowerThirdsStartTime);
     lowerThirdsClip.end = lowerThirdsClip.start.seconds + 10;
 
@@ -90,7 +105,7 @@ function createNewProject(nameOfTandem, currentDate, hasBoard) {
     //Copyrights
     var copyrightsMogrtPath = mogrtFolder + "/" + "Copyrights.mogrt";
     var copyrightsMogrtFile = new File(copyrightsMogrtPath);
-    var outroClip = vTrackTwo.clips[vTrackTwo.clips.length - 1]
+    var outroClip = ap.vTrackTwo.clips[ap.vTrackTwo.clips.length - 1]
     var copyrightsStartTime = outroClip.end.seconds - 8;
     var copyrightClip = insertMogrts(copyrightsMogrtFile, copyrightsStartTime);
     copyrightClip.end = outroClip.end;
@@ -100,150 +115,66 @@ function createNewProject(nameOfTandem, currentDate, hasBoard) {
     // ------------------------------- Create New Project Functions ---------------------------- //
     // ----------------------------------------------------------------------------------------- //
 
-
-    function createTandemFolder(nameOfTandem, parentFolderObj) {
-        //Create folder for the tandem
-        var tandemFolder = parentFolderObj + "/" + nameOfTandem;
-        var tandemFolderObj = new Folder(tandemFolder);
-        if (tandemFolderObj.exists) {
-            alert("Tandem Folder Name already exists", "Error: Duplicate Folder", true);
-            return "error";
-        }
-        tandemFolderObj.create();
-
-        //Create Videos folder inside the tandem folder
-        var videosFolder = tandemFolder + "/Videos";
-        var videosFolderObj = new Folder(videosFolder);
-        videosFolderObj.create();
-
-        return [tandemFolderObj, videosFolderObj];
-    }
-
-    function importTandem(tandemProjectFilePath, videosFolderObj, hasBoard) {
-        var copiedFiles = [];
-
-        //Run File Select
-        var fileSelected = utils.fileSelector();
-
-        if (fileSelected) {
-            //Check if number of selection is valid
-            var isSelectionValid = (!hasBoard && fileSelected.length >= 4) || (hasBoard && fileSelected.length >= 5);
-            if (isSelectionValid) {
-                //Create New Premiere Pro Project File (Prproj)
-                app.newProject(tandemProjectFilePath);
-
-                //Create Bins
-                utils.activeProjectInitialize();
-                projRoot.createBin("Template");
-                projRoot.createBin("Videos");
-                projRoot.createBin("Sub Clips");
-                utils.binInitialize();
-            } else {
-                alert("If the board exists, select a minimum of 5 files; otherwise, select a minimum of 4 files.", "Error: Invalid Selection", true);
-                return "Invalid Selection";
-            }
-        } else {
-            return "Invalid Selection";
-        }
-
-        //Copy tandem files to tandem videos folder
-        if (videosFolderObj.getFiles().length > 0) {
-            return;
-        } else {
-            for (var i in fileSelected) {
-                fileSelected[i].copy(videosFolderObj + '/' + fileSelected[i].displayName);
-            }
-        }
-
-        //Import copied files to PrPro
-        for (var i in videosFolderObj.getFiles()) {
-            copiedFiles.push(videosFolderObj.getFiles()[i].fsName)
-        }
-        proj.importFiles(copiedFiles, false, vidBin, false);
-    }
-
-    function createFhdSeq(nameOfTandem, presetPath) {
-        proj.newSequence(nameOfTandem + " - FHD", presetPath.fsName);
+    function createFhdSeq(tandemName, presetPath) {
+        ap.proj.newSequence(tandemName + " - FHD", presetPath.fsName);
     }
 
     function renameProjectFiles(hasBoard) {
         if (hasBoard) {
-            board.name = "Board";
+            ap.board.name = "Board";
         }
-        preInterview.name = "PreInterview";
-        walkToPlane.name = "WalkToPlane";
-        freefall.name = "Freefall";
-        if (hasLanding) {
-            landing.name = "Landing"
+        ap.preInterview.name = "PreInterview";
+        ap.walkToPlane.name = "WalkToPlane";
+        ap.freefall.name = "Freefall";
+        if (ap.hasLanding) {
+            ap.landing.name = "Landing"
         }
-        for (var i in postInterview) {
-            postInterview[i].name = "PostInterview";
+        for (var i in ap.postInterview) {
+            ap.postInterview[i].name = "PostInterview";
         }
     }
 
     function insertFhdClips() {
         //Insert Intro Template
-        vTrackTwo.insertClip(fhdIntro, 0);
-        vTrackTwo.insertClip(fhdIntroMask, vTrackTwo.clips[0].end);
-        aTrackTwo.overwriteClip(fhdIntroSound, 0)
+        ap.vTrackTwo.insertClip(ap.fhdIntro, 0);
+        ap.vTrackTwo.insertClip(ap.fhdIntroMask, ap.vTrackTwo.clips[0].end);
+        ap.aTrackTwo.overwriteClip(ap.fhdIntroSound, 0)
 
         //Insert Tandem Videos and Stock Shots
-        vTrackOne.insertClip(preInterview, vTrackTwo.clips[1].start);
-        vTrackOne.insertClip(fhdStockOne, vTrackOne.clips[0].end);
-        vTrackOne.insertClip(walkToPlane, vTrackOne.clips[1].end);
-        vTrackOne.insertClip(fhdStockTwo, vTrackOne.clips[2].end);
-        vTrackOne.insertClip(freefall, vTrackOne.clips[3].end);
-        if (hasLanding) {
-            vTrackOne.insertClip(landing, vTrackOne.clips[4].end);
-            for (var i = 0; i < postInterview.length; i++) {
-                vTrackOne.insertClip(postInterview[i], vTrackOne.clips[5 + i].end);
+        ap.vTrackOne.insertClip(ap.preInterview, ap.vTrackTwo.clips[1].start);
+        ap.vTrackOne.insertClip(ap.fhdStockOne, ap.vTrackOne.clips[0].end);
+        ap.vTrackOne.insertClip(ap.walkToPlane, ap.vTrackOne.clips[1].end);
+        ap.vTrackOne.insertClip(ap.fhdStockTwo, ap.vTrackOne.clips[2].end);
+        ap.vTrackOne.insertClip(ap.freefall, ap.vTrackOne.clips[3].end);
+        if (ap.hasLanding) {
+            ap.vTrackOne.insertClip(ap.landing, ap.vTrackOne.clips[4].end);
+            for (var i = 0; i < ap.postInterview.length; i++) {
+                ap.vTrackOne.insertClip(ap.postInterview[i], ap.vTrackOne.clips[5 + i].end);
             }
         } else {
-            for (var i = 0; i < postInterview.length; i++) {
-                vTrackOne.insertClip(postInterview[i], vTrackOne.clips[4 + i].end);
+            for (var i = 0; i < ap.postInterview.length; i++) {
+                ap.vTrackOne.insertClip(ap.postInterview[i], ap.vTrackOne.clips[4 + i].end);
             }
         }
 
         //Initialize tandem video track clips inside an array variable
-        utils.clipsToVarInitialize();
+        ap.clipsToVarInitialize();
 
         //Determine the last clip in video track one
-        var lastInterviewClip = postInterviewVClip[postInterviewVClip.length - 1];
+        var lastInterviewClip = ap.postInterviewVClip[ap.postInterviewVClip.length - 1];
 
         //Insert Outro Template
-        vTrackTwo.insertClip(fhdOutroMask, lastInterviewClip.end.seconds - .65);
-        vTrackTwo.insertClip(fhdOutro, vTrackTwo.clips[2].end);
+        ap.vTrackTwo.insertClip(ap.fhdOutroMask, lastInterviewClip.end.seconds - .65);
+        ap.vTrackTwo.insertClip(ap.fhdOutro, ap.vTrackTwo.clips[2].end);
     }
 
     function insertMogrts(mogrtFile, startTime) {
-        var mogrtClip = seq.importMGT(mogrtFile.fsName, startTime, 3, 0);
+        var mogrtClip = ap.seq.importMGT(mogrtFile.fsName, startTime, 3, 0);
         return mogrtClip;
     }
 }
 
-function importHandicam() {
-    var copiedFiles = [];
-    var folderPathObj = new Folder(parentFolderObj);
 
-    var fileSelected = fileSelector();
-
-    if (fileSelected) {
-        proj.rootItem.createBin("HC Videos");
-        utils.hcBinInitialize();
-    } else {
-        return;
-    }
-
-    for (var i in fileSelected) {
-        fileSelected[i].copy(parentFolderObj + '/' + fileSelected[i].displayName);
-    }
-
-    var copiedFilesObj = folderPathObj.getFiles();
-
-    for (var i in copiedFilesObj) {
-        copiedFiles.push(copiedFilesObj[i].fsName)
-    }
-
-
-    proj.importFiles(copiedFiles, false, hcBin, false);
+function insertHandicam() {
+    var ap = new ActiveProject();
 }
--- jsx/utils.jsx
+++ jsx/utils.jsx
@@ -1,160 +1,202 @@
-﻿//Global Variable Declarations
-var proj, projRoot, projPath;
+﻿function ActiveProject() {
 
-var masterBin, vidBin, templateBin, subClipsBin, hcBin;
+}
 
-var seq, vTrack, aTrack, vTrackOne, vTrackTwo, vTrackThree, aTrackOne, aTrackTwo, aTrackThree;
+//Initialize active project
+ActiveProject.prototype.InitializeCurrentProject = function() {
+    this.proj = app.project;
+    this.projPath = app.project.path;
+    this.projRoot = app.project.rootItem;
+    this.masterBin = app.project.rootItem.children;
+};
 
-var vidBinFiles, numOfVidClips, hasLanding, board, preInterview, walkToPlane, freefall, landing, postInterview = [];
+//Initialize sequence and tracks for the active project
+ActiveProject.prototype.activeSeqAndTracksInitialize = function() {
+    this.seq = app.project.activeSequence;
+    this.vTrack = this.seq.videoTracks;
+    this.vTrackOne = this.vTrack[0];
+    this.vTrackTwo = this.vTrack[1];
+    this.vTrackThree = this.vTrack[2];
+    this.aTrack = this.seq.audioTracks;
+    this.aTrackOne = this.aTrack[0];
+    this.aTrackTwo = this.aTrack[1];
+    this.aTrackThree = this.aTrack[2];
+};
 
-var fhdIntro, fhdIntroMask, fhdStockOne, fhdStockTwo, fhdOutro, fhdOutroMask, fhdIntroSound, fhdLowerThirds, fhdWatermark, fhdMusic;
-
-var preInterviewVClip = [], walkToPlaneVClip = [], freefallVClip = [], landingVClip = [], postInterviewVClip = []; 
-
-var srcFolder = Folder.myDocuments + "/" + "SD Extension Presets";
-
-
-var utils = {
-    //Select tandem files from system
-    fileSelector: function() {
-        var prompt = "Select Tandem Videos";
-        var filter = "*mp4;*.jpg;*.jpeg;*mov;*avi;*png";
-        var multiSelect = true;
-
-        var fileSelected = File.openDialog(prompt, filter, multiSelect);
-        return fileSelected;
-    },
-
-    //Set bin variables for the active project
-    binInitialize: function() {
-        for (var i = 0; i < masterBin.length; i++) {
-            if (masterBin[i].name == "Template") {
-                templateBin = masterBin[i];
-            } else if (masterBin[i].name == "Videos") {
-                vidBin = masterBin[i];
-            } else if (masterBin[i].name == "Sub Clips") {
-                subClipsBin = masterBin[i];
-            }
-        }
-    },
-
-    //Set handicam bin variable for the active project
-    hcBinInitialize: function() {
-        for (var i = 0; i < masterBin.length; i++) {
-            if (masterBin[i].name == "HC Videos") {
-                hcBin = masterBin[i];
-            }
-        }
-    },
-
-    //Set jsx ppro variables for the active project
-    activeProjectInitialize: function() {
-        proj = app.project;
-        projPath = proj.path;
-        projRoot = proj.rootItem;
-        masterBin = projRoot.children;
-    },
-
-    activeSeqAndTracksInitialize: function() {
-        seq = proj.activeSequence;
-        vTrack = seq.videoTracks;
-        vTrackOne = vTrack[0];
-        vTrackTwo = vTrack[1];
-        vTrackThree = vTrack[2];
-        aTrack = seq.audioTracks;
-        aTrackOne = aTrack[0];
-        aTrackTwo = aTrack[1];
-        aTrackThree = aTrack[2];
-    },
-
-    //Check the prproj file name
-    fileNameChecker: function() {
-        var parts = projPath.split('\\');
-        var fileNameWithExtension = parts[parts.length - 1];
-        var fileNameWithoutExtension = fileNameWithExtension.split('.')[0];
-
-        return fileNameWithoutExtension;
-    },
-
-    //Assign Tandem Videos to Variables
-    fhdVidsToVarInitialize: function(hasBoard) {
-        vidBinFiles = vidBin.children;
-        numOfVidClips = vidBinFiles.length;
-        hasLanding = false;
-        if (hasBoard) {
-            board = vidBinFiles[0];
-            preInterview = vidBinFiles[1];
-            walkToPlane = vidBinFiles[2];
-            freefall = vidBinFiles[3];
-            if (numOfVidClips > 5) {
-                landing = vidBinFiles[4];
-                hasLanding = true;
-            }
-            var postInterviewStart = hasLanding ? 5 : 4;
-            for (var i = postInterviewStart; i < numOfVidClips; i++) {
-                postInterview.push(vidBinFiles[i]);
-            }
-        } else {
-            preInterview = vidBinFiles[0];
-            walkToPlane = vidBinFiles[1];
-            freefall = vidBinFiles[2];
-            if (numOfVidClips > 4) {
-                landing = vidBinFiles[3];
-                hasLanding = true;
-            }
-            var postInterviewStart = hasLanding ? 4 : 3;
-            for (var i = postInterviewStart; i < numOfVidClips; i++) {
-                postInterview.push(vidBinFiles[i]);
-            }
-        }
-    },
-
-    importStock: function(folder, bin) {
-        var stockFiles = []
-        for (var i in folder.getFiles()) {
-            stockFiles.push(folder.getFiles()[i].fsName)
-        }
-
-        proj.importFiles(stockFiles, false, bin, false);
-    },
-
-    fhdTemplateToVarInitialize: function() {
-        var templateBinItems = templateBin.children;
-        fhdIntro = templateBinItems[0];
-        fhdIntroMask = templateBinItems[1];
-        fhdStockOne = templateBinItems[2];
-        fhdStockTwo = templateBinItems[3];
-        fhdOutroMask = templateBinItems[4];
-        fhdOutro = templateBinItems[5];
-        fhdIntroSound = templateBinItems[6];
-        fhdLowerThirds = templateBinItems[7];
-        fhdWatermark = templateBinItems[8];
-        fhdMusic = templateBinItems[9];
-    },
-
-    convertToSubclip: function(clip) {
-        var subClip = clip.createSubClip(clip.name + " - subclip", clip.getInPoint().ticks, clip.getOutPoint().ticks, 0, 1, 0);
-        subClip.moveBin(subClipsBin);
-        return subClip;
-    },
-
-    clipsToVarInitialize: function() {
-        preInterviewVClip = [], walkToPlaneVClip = [], freefallVClip = [], landingVClip = [], postInterviewVClip = [];
-        for (var i = 0; i < vTrackOne.clips.length; i++){
-            var clip = vTrackOne.clips[i];
-            var clipName = clip.name.toLowerCase();
-            if (clipName.indexOf("preinterview") != -1){
-                preInterviewVClip.push(clip);
-            } else if (clipName.indexOf("walktoplane") != -1) {
-                walkToPlaneVClip.push(clip);
-            } else if (clipName.indexOf("freefall") != -1) {
-                freefallVClip.push(clip);
-            } else if (clipName.indexOf("landing") != -1) {
-                landingVClip.push(clip);
-            } else if (clipName.indexOf("postinterview") != -1) {
-                postInterviewVClip.push(clip);
-            }
-                
+ActiveProject.prototype.getBin = function(binName) {
+    for (var i = 0; i < this.masterBin.length; i++) {
+        if (this.masterBin[i].name == binName) {
+            return this.masterBin[i];
         }
     }
+    return null;
+};
+
+//Initialize fhd bin for the active project
+ActiveProject.prototype.fhdBinInitialize = function() {
+    this.templateBin = this.getBin("Template");
+    this.vidBin = this.getBin("Videos");
+    this.subClipsBin = this.getBin("Sub Clips");
+};
+
+//Initialize handicam bin for the active project
+ActiveProject.prototype.hcBinInitialize = function() {
+    this.hcBin = this.getBin("HC Videos");
+};
+
+//Check the prproj file name
+ActiveProject.prototype.getFileName = function() {
+    var parts = this.projPath.split('\\');
+    var fileNameWithExtension = parts[parts.length - 1];
+    var fileNameWithoutExtension = fileNameWithExtension.split('.')[0];
+
+    return fileNameWithoutExtension;
+};
+
+ActiveProject.prototype.importToProject = function(files, bin) {
+    var filesToImport = []
+    for (var i in files) {
+        filesToImport.push(files[i].fsName)
+    }
+    app.project.importFiles(filesToImport, false, bin, false);
 }
+
+ActiveProject.prototype.selectionValidator = function(selectionType, selectionLength, hasBoard) {
+    if (selectionType === "Videos") {
+        var validator = (!hasBoard && selectionLength >= 4) || (hasBoard && selectionLength >= 5);
+        var alertDesc = "If the board exists, select a minimum of 5 files; otherwise, select a minimum of 4 files."
+    } else if (selectionType === "Handicam") {
+        var validator = false;
+    } else {
+        var validator = false;
+    }
+
+    if (!validator) {
+        alert(alertDesc, "Error: Invalid Selection", true);
+        return false;
+    } else {
+        return validator;
+    }
+}
+
+ActiveProject.prototype.copyFilesToSubFolder = function(files, folderName) {
+    //Create sub-folder
+    var destFolder = this.createNewFolder(folderName);
+
+    //Copy tandem files to sub-folder
+    for (var i in files) {
+        files[i].copy(destFolder + '/' + files[i].displayName);
+    }
+
+    return destFolder;
+}
+
+//Assign Tandem Videos to Properties
+ActiveProject.prototype.fhdVidsToVarInitialize = function(hasBoard) {
+    var vidBinFiles = this.vidBin.children;
+    this.numOfVidClips = vidBinFiles.length;
+    this.hasLanding = false;
+    this.postInterview = [];
+    if (hasBoard) {
+        this.board = vidBinFiles[0];
+        this.preInterview = vidBinFiles[1];
+        this.walkToPlane = vidBinFiles[2];
+        this.freefall = vidBinFiles[3];
+        if (this.numOfVidClips > 5) {
+            this.landing = vidBinFiles[4];
+            this.hasLanding = true;
+        }
+        var postInterviewStart = this.hasLanding ? 5 : 4;
+        for (var i = postInterviewStart; i < this.numOfVidClips; i++) {
+            this.postInterview.push(vidBinFiles[i]);
+        }
+    } else {
+        this.preInterview = vidBinFiles[0];
+        this.walkToPlane = vidBinFiles[1];
+        this.freefall = vidBinFiles[2];
+        if (this.numOfVidClips > 4) {
+            this.landing = vidBinFiles[3];
+            this.hasLanding = true;
+        }
+        var postInterviewStart = this.hasLanding ? 4 : 3;
+        for (var i = postInterviewStart; i < this.numOfVidClips; i++) {
+            this.postInterview.push(vidBinFiles[i]);
+        }
+    }
+};
+
+//Assign Stock Files to Properties
+ActiveProject.prototype.fhdTemplateToVarInitialize = function() {
+    var templateBinItems = this.templateBin.children;
+    this.fhdIntro = templateBinItems[0];
+    this.fhdIntroMask = templateBinItems[1];
+    this.fhdStockOne = templateBinItems[2];
+    this.fhdStockTwo = templateBinItems[3];
+    this.fhdOutroMask = templateBinItems[4];
+    this.fhdOutro = templateBinItems[5];
+    this.fhdIntroSound = templateBinItems[6];
+    this.fhdLowerThirds = templateBinItems[7];
+    this.fhdWatermark = templateBinItems[8];
+    this.fhdMusic = templateBinItems[9];
+};
+
+//Create subclips to remove audio
+ActiveProject.prototype.convertToSubclip = function(clip) {
+    var subClip = clip.createSubClip(clip.name + " - subclip", clip.getInPoint().ticks, clip.getOutPoint().ticks, 0, 1, 0);
+    subClip.moveBin(this.subClipsBin);
+    return subClip;
+};
+
+//Assign active fhd clips to properties
+ActiveProject.prototype.clipsToVarInitialize = function() {
+    this.preInterviewVClip = [], this.walkToPlaneVClip = [], this.freefallVClip = [], this.landingVClip = [], this.postInterviewVClip = [];
+    for (var i = 0; i < this.vTrackOne.clips.length; i++) {
+        var clip = this.vTrackOne.clips[i];
+        var clipName = clip.name.toLowerCase();
+        if (clipName.indexOf("preinterview") != -1) {
+            this.preInterviewVClip.push(clip);
+        } else if (clipName.indexOf("walktoplane") != -1) {
+            this.walkToPlaneVClip.push(clip);
+        } else if (clipName.indexOf("freefall") != -1) {
+            this.freefallVClip.push(clip);
+        } else if (clipName.indexOf("landing") != -1) {
+            this.landingVClip.push(clip);
+        } else if (clipName.indexOf("postinterview") != -1) {
+            this.postInterviewVClip.push(clip);
+        }
+
+    }
+};
+
+//Universal File Select Function
+ActiveProject.prototype.fileSelector = function() {
+    var prompt = "Select Tandem Videos";
+    var filter = "*mp4;*.jpg;*.jpeg;*mov;*avi;*png";
+    var multiSelect = true;
+
+    var fileSelected = File.openDialog(prompt, filter, multiSelect);
+    return fileSelected;
+};
+
+ActiveProject.prototype.createNewFolder = function(folderName) {
+    parentFolder = this.getParentFolder();
+    var newFolder = new Folder(parentFolder + "/" + folderName);
+    if (newFolder.exists) {
+        alert("Folder Name already exists", "Error: Duplicate Folder", true);
+        return null;
+    } else {
+        newFolder.create();
+        return newFolder;
+    }
+};
+
+//Declare source folder for templates and presets
+ActiveProject.prototype.getSrcFolder = function() {
+    return new Folder(Folder.myDocuments + "/SD Extension Presets");
+};
+
+//Get the Parent Folder Path of the Current Project
+ActiveProject.prototype.getParentFolder = function() {
+    var parentFolderPath = app.project.path.substring(0, app.project.path.lastIndexOf("\\") + 1);
+    return new Folder(parentFolderPath);
+};
